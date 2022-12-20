b428b6dc4bb4faaddab1d4adba9f7962724bcc5b
Roxtaru <roxtaru@gmail.com>
2022-12-20 12:17:34 -0800

    day2.py

--- day2.py
+++ day2.py
@@ -1,4 +1,45 @@
 # A - Rock B - Paper C - Scissors
-# X - Rock Y - Paper Z - Scissors
+# X - LOSE Y - DRAW Z - WIN
 # 1 - Rock 2 - Paper 3 - Scissors 
 # 0 - Lost 6 - Win 3 - Draw 
+
+def main():
+    totalWin = 0
+    
+    with open("day2input.txt") as f:
+        lines = f.readlines()
+        f.close()
+                
+    for i in lines:
+        if i[2] == 'X':
+            if i[0] == 'A':
+                totalWin += 3
+            if i[0] == 'B':
+                totalWin += 1
+            if i[0] == 'C':
+                totalWin += 2
+        if i[2] == 'Y':
+            totalWin += 3
+            if i[0] == 'A':
+                totalWin += 1
+            if i[0] == 'B':
+                totalWin += 2
+            if i[0] == 'C':
+                totalWin += 3    
+        if i[2] == 'Z':
+            totalWin += 6
+            if i[0] == 'A':
+                totalWin += 2
+            if i[0] == 'B':
+                totalWin += 3
+            if i[0] == 'C':
+                totalWin += 1    
+            
+            
+    print(totalWin)
+        
+ 
+ 
+    
+if __name__ == "__main__":
+    main()
